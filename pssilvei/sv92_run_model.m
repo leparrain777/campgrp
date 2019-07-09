78be1497a67379cdd3c1217b6b863b68e231909c
leparrain777 <30747873+leparrain777@users.noreply.github.com>
2019-07-09 13:43:03 -0700

    Update sv92_run_model.m

--- pssilvei/sv92_run_model.m
+++ pssilvei/sv92_run_model.m
@@ -23,10 +23,13 @@ options = odeset('Events',@sm91_co2_events);
 xprime(:,1) = xprime(:,1).*2.0;
 xprime(:,2) = xprime(:,2).*52.5;
 xprime(:,3) = xprime(:,3).*0.9;
+xprime(:,4) = xprime(:,4).*1.0;
 
 ye(:,1) = ye(:,1).*2.0;
 ye(:,2) = ye(:,2).*52.5;
 ye(:,3) = ye(:,3).*0.9;
+ye(:,4) = ye(:,4).*1.0;
+
 
 % Add the tectonic-average equilibrium solution to the Pleistocene departure model 
 % to get the full solution for every value of t.
@@ -38,10 +41,12 @@ for i = 1:size(t)
    x(1,i) = xprime(i,1);
    x(2,i) = xprime(i,2);
    x(3,i) = xprime(i,3);
+   x(4,i) = xprime(i,3);
    
    xfull(1,i) = xprime(i,1) + tectsol(1);
    xfull(2,i) = xprime(i,2) + tectsol(2);
    xfull(3,i) = xprime(i,3) + tectsol(3);
+   xfull(4,i) = xprime(i,4) + tectsol(4);
 end
 
 % Separate out solutions.
@@ -49,14 +54,14 @@ end
 I = squeeze(x(1,:))';
 Mu = squeeze(x(2,:))';
 Theta = squeeze(x(3,:))';
-
+D = squeeze(x(4,:))';
 
 xfull = xfull';
-D = [te ye;t I Mu Theta];
-%Dfull = [t xfull];
+Data = [te ye;t I Mu Theta D];
+%Datafull = [t xfull];
 
-%storeData(D,fileName,filePath,4);
-%storeData(Dfull,fileName2,filePath,4);
+%storeData(Data,fileName,filePath,4);
+%storeData(Datafull,fileName2,filePath,4);
 
 toc
 
