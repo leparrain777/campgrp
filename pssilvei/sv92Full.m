c5b42476ee7e34fd4c7257c75c8eb22775e63da2
leparrain777 <30747873+leparrain777@users.noreply.github.com>
2019-07-20 01:50:05 -0700

    Struct makes code lightspeed

--- pssilvei/sv92Full.m
+++ pssilvei/sv92Full.m
@@ -10,31 +10,29 @@
 
 %need co2events and tectonic
 
-function xprime = sm92(t,x,param,parT,R,S,Rt,Rx,Ry,Rz,Rw,insolT,insol,timescale,massscale,distancescale,co2scale,tempscale);
+function xprime = sv92(t,x,params);
 
-p = param(1);
-q = param(2);
-u = param(3);
-v = param(4);
+% p = param(1);
+% q = param(2);
+% u = param(3);
+% v = param(4);
 
 %r = interp1(parT,R,t,'pchip');
 %s = interp1(parT,S,t,'pchip');
-r = 1.3;
-s = 0.6;
+% r = 1.3;
+% s = 0.6;
 
-% Insolation:
-standarddeviationmultiplier = 25;
-Rprime = standarddeviationmultiplier*interp1(insolT,insol,t,'spline');
+
 
 % Stochastic Terms:
 %Wx = interp1(Rt,Rx,t,'pchip');
 %Wy = interp1(Rt,Ry,t,'pchip');
 %Wz = interp1(Rt,Rz,t,'pchip');
 %Ww = interp1(Rt,Rw,t,'pchip');
-Wx = 0;
-Wy = 0;
-Wz = 0;
-Ww = 0;
+% Wx = 0;
+% Wy = 0;
+% Wz = 0;
+% Ww = 0;
 
 
 
@@ -44,8 +42,87 @@ D = x(2);
 muprime = x(3);
 thetaprime = x(4);
 
+mu = muprime + params.munot;
 
-functiongeneratorforsm92
+ %I=globalicemass;
+%make I a short name for globalicemass as of model time
+
+theta= params.thetanot + thetaprime;
+%make theta equal to the baseline value plus the drifting value
+
+Rprime = params.standarddeviationmultiplier * ppval(params.Rprime,t);
+
+R = params.Rnot + Rprime;
+%make R a short name for high latitude radiation as of model time
+
+
+
+%highlatradiation = insol*90;
+
+
+%Rprime = interp1([0 : 1000 : 5e6],highlatradiation(t) - 452,t,'pchip');
+%Rprime = highlatradiation - 452;
+%make Rprime equal to the baseline value minus the drifting value
+
+
+%make alphafour a short nam for the rate of ice destruction
+
+
+
+
+psiprime = psi - params.psinot;
+%make psi equal to the baseline value plus the drifting value
+
+%JI =@() 2.67e-18;
+%Jtheta=@() 1;
+%syms Z; Z = Znotstar + JI * psi+ Znotstar * Jtheta * (theta - thetastar);
+
+
+
+ H = nthroot(params.zeta^4 * psi / (params.n * params.icedensity),5);
+%setting the mean thickness of ice sheets, and creating a short name H
+
+
+Dnot = 1/3 * H; % epsilonone / epsilontwo basically 1/3 H by paper
+% setting the threshold level of bedrock depression for ice calving
+
+%Rprime = R - Rnot;
+
+
+
+C = -params.alphafour * psi / (H * params.n); %C = matlabFunction(piecewise(D < Z | D < Dnot, 0, D > Z & D > Dnot, -alphafour * psi / (H * n)));
+%computes the value of C from other items that should be given in the
+%differential equation
+
+Cflag = double((D > params.Z) & (D > Dnot)); %this is our alternative to a piecewise function
+%and is a result of the function being zero if certain conditions are or are not met.
+
+omegaI = params.omegaI;
+omegamu = params.omegamu;
+omegatheta = params.omegatheta;
+
+
+
+equation11 = params.alphanot - params.alphatwo * (params.c * muprime + params.kappatheta * thetaprime + params.kappaR * Rprime) - params.alphathree * psi + params.n * C*Cflag + omegaI;
+%matlabFunction(su(equation11))
+%creating a version of equation 11 using pieces of equations that we have
+%already written.
+
+equation12 = params.eone * nthroot(psi,5) - params.epsilontwo * D;
+%matlabFunction(su(equation12))
+%creating a version of equation 12 using pieces of equations that we have
+%already written.
+
+equation13 = muprime * (params.bone - params.btwo * muprime - params.bthree * muprime^2) - params.bfour * thetaprime + omegamu;
+%matlabFunction(su(equation13))
+%creating a version of equation 13 using pieces of equations that we have
+%already written.
+
+equation14 = params.gammanot - params.gammatwo * psi - params.gammathree * thetaprime + omegatheta;
+%matlabFunction(su(equation14))
+%creating a version of equation 14 using pieces of equations that we have
+%already written.
+
 
 
 if psi<=0
--- pssilvei/sv92_params.m
+++ pssilvei/sv92_params.m
@@ -27,6 +27,9 @@ timescale = 1000; %years
 % Here, the simulation runs for 5 million years ago to present.
 tspan = [0:1*1e3/timescale:5e6/timescale];
 
+
+
+
 % Initial conditions are chosen arbitrarily based loosely on the Saltzman 1990 paper.
 %x0 = [-1.0 0.1 1.0];
 x0 = [.001 .001 .001 .001];
@@ -57,10 +60,10 @@ if Laskarflag
    [insolT,~,~,~,insol] = readLaskarAstronomical(0,5000);
 
    %insolT = insolT./10;
-   insolT = [0:1000/timescale:5e6/timescale];
+   params.insolT = [0:1000/timescale:5e6/timescale];
 
-   insol = (insol - mean(insol))/(std(insol));
-   insol = insol(end:-1:1);
+   params.insol = (insol - mean(insol))/(std(insol));
+   params.insol = params.insol(end:-1:1);
 
 elseif integInsolflag
    descr = 'insolHuybersIntegrated';
@@ -98,19 +101,137 @@ elseif unforcedFlag
 
 end %if
 
+%18 only
+%setting the sensitivity to high latitude surface temp constant one aka b
+params.b = 18 / tempscale ;
+%make b a short name for snsitivivityhighlatsurfacetempone
+
+%4e-3 only
+%setting the sensitivity to high latitude surface temp constant two aka c
+params.c = 4e-3 * co2scale ;
+%make c a short name for snsitivivityhighlatsurfacetemptwo
+
+
+
+%the current value of high latitude radiation
+
+%externalforcingcarbondioxide=[1 2 3 4];
+%the forcing signal on carbon dioxid for each timestep of the run, should
+%be calculated by something else, this is a placeholder
+
+%externalforcingoceantemp=[1 2 3 4];
+%the forcing signal on ocean temperature for each timestep of the run, should
+%be calculated by something else, this is a placeholder
+
+params.alphaone= timescale*15.4e15 / massscale; %13.7e15 or 15.4e15
+params.alphatwo= timescale*9.4e15 / massscale; %7.0e15 or 9.4e15
+params.alphathree= timescale*1.0e-4; %1.0e-4
+params.alphafour= timescale*20 / distancescale; %20 or 0 aka alphafour
+%setting the rate of ice destruction
+%setting alpha rate constants
+
+params.bone= timescale*1.3e-4; %1.3e-4 or 0 
+params.btwo= timescale*1.1e-6 * co2scale; % 1.1e-6 or 0
+params.bthree= timescale*3.6e-8 * co2scale^2; % 0 or 3.6e-8
+params.bfour= timescale*5.6e-3 / co2scale * tempscale; %0 or 5.6e-3
+%setting b constants
+
+params.gammaone= timescale*1.9e-3 / tempscale; %1.9e-3 or 0
+params.gammatwo= timescale*1.2e-23 / tempscale * massscale; %0 or 1.2e-23
+params.gammathree= timescale*2.5e-4; %0 or 2.5e-4
+%setting gamma rate constants
+
+params.kappaR= 1.1e-2 / distancescale^2; %.7e-2 or 1.1e-2 or 1.7e-2
+params.kappatheta= 4.4e-2 * tempscale; %3.3e-2 or 4.4e-2
+%setting kappa constants
+
+params.Kmu= 0 * massscale / co2scale;%2e-18; %2e-18 or possibly 0?
+params.Ktheta= 0 * massscale / tempscale;%4.8e-20; %4.8e-20 or possibly 0?
+%setting K constants for pollard paper emulation
+
+params.munotstar= 253 / co2scale; %253 or 250 or 215
+params.thetanotstar= 5.2 / tempscale; %5.2 or 4.8
+%setting current atmosphere averages
+
+params.Istar= 3e19 / massscale; %3e19 or 3.3e19
+%setting the present value of global ice mass
+
+
+params.Z= 4e2 / distancescale; %4e2 or 0 or 6.4e2
+%Znot=Znotstar;
+%setting the baseline value of tectonic crust equilibrium to be the modern tectonic crust equilibrium?
+
+params.epsilontwo= timescale*1/(30e3); %1/(3e3) or 1/(30e3)
+%setting epsilon 2. It is often stated by its inverse in the paper.
+
+params.epsilonone = params.epsilontwo * 1/3; %this should be epsilonone *1/3 as they use a 
+%constant in the paper for epsilon one divided by epsilon two with value one third
+
+params.zeta= 1 / nthroot(distancescale,2); %1 or .5
+%setting zeta constant
+
+params.icedensity= 917 / massscale; %917 given in paper
+%set ice density, seen as rho with an i subscript
+
+params.n= 2; % 2 in paper
+%set number of ice sheets to consider 
+
+params.omegamu = 0; %0 in the paper
+%set the forcing term for mu
+
+params.omegatheta = 0; %0 in the paper
+%set the forcing term for theta
+
+params.omegaI = 0; %0 in paper when not explicityly stated otherwise
+%set the forcing term for I/phi
+
+format long e
+%set our outputs to have some more decimals and a seperate magnitude
+%multiplier
+
+
+params.munot = params.munotstar; %
+%look up
+params.thetanot = params.thetanotstar;
+
+params.Rstar = 500; % NOTE: this is wrong, it is a parameter
+%make Rstar a short name for high latitude radiation present value
+%look up
+
+params.Rnotstar = 452;
+params.Rnot = params.Rnotstar;
+%look up
+%setting some placeholder values for testing to be replaced later
+params.gammanot = params.gammaone - params.gammatwo * params.Istar - params.gammathree * params.thetanot;
+%gammanot = gammatwo * psinot; %= gammatwo * alphanot / alphathree as phinot = alphnot / alphathree if muprime = thetaprime = 0;
+
+params.psinot = params.gammanot/params.gammatwo; 
+
+params.alphanot = params.alphaone - params.alphatwo * tanh(params.c * params.munot) + params.kappatheta * params.thetanot + params.kappaR * (params.Rnot - params.Rstar) - params.alphathree * params.Istar;
+%computes the value of alphanot from other items that are given
+
+
+
+params.eone= params.epsilonone * nthroot(params.zeta^4/(params.icedensity * params.n),5);
+%computes the value of eone from other items that are given
+
+% Insolation:
+params.standarddeviationmultiplier = 25;
+params.Rprime = interp1(params.insolT,params.insol,'spline','pp');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Set up the changing parameters:
 % Note: These changing parameters depend on the fact that the model runs for 5 million years.
-parT = [0:1:5000]';
-U = -35*(parT/100) + 425;
-R = (2*2.6*10^(-5).*U - 3*3.6*10^(-8)*(U.^2) - 6.3*10^(-3)) / (1*10^(-4)) + 2;
-S = (3*3.6*10^(-8).*U - 2.6*10^(-5)) / (sqrt(1*10^(-4)*3.6*10^(-8))); 
-
-% Set up the stochastic terms:
-Rt = [0:.1:500]';
-Rx = -0.025 + 0.05.*rand(5001,1);
-Ry = -0.025 + 0.05.*rand(5001,1);
-Rz = -0.025 + 0.05.*rand(5001,1);
-Rw =  -0.025 + 0.05.*rand(5001,1);
+% parT = [0:1:5000]';
+% U = -35*(parT/100) + 425;
+% R = (2*2.6*10^(-5).*U - 3*3.6*10^(-8)*(U.^2) - 6.3*10^(-3)) / (1*10^(-4)) + 2;
+% S = (3*3.6*10^(-8).*U - 2.6*10^(-5)) / (sqrt(1*10^(-4)*3.6*10^(-8))); 
+% 
+% % Set up the stochastic terms:
+% Rt = [0:.1:500]';
+% Rx = -0.025 + 0.05.*rand(5001,1);
+% Ry = -0.025 + 0.05.*rand(5001,1);
+% Rz = -0.025 + 0.05.*rand(5001,1);
+% Rw =  -0.025 + 0.05.*rand(5001,1);
 
 % Storing Data info
 %fileName = sprintf('SM91_%s_%d_Model.txt',descr,runID);
