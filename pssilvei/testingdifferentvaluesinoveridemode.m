18134df3919732a2ffe9f9d2157608f1ae29b051
leparrain777 <30747873+leparrain777@users.noreply.github.com>
2019-07-24 11:58:35 -0700

    Update testingdifferentvaluesinoveridemode.m

--- pssilvei/testingdifferentvaluesinoveridemode.m
+++ pssilvei/testingdifferentvaluesinoveridemode.m
@@ -1,4 +1,6 @@
 
-parfor hello = 1:16
-    bigun(:,:,hello) = sv92_run_model((hello + 34) *10);
+parfor hello = 1:10
+    bigun(:,:,hello) = sv92_run_model((hello + 450) *1);
 end
+%unforced sudden 500 integers, forced gradual but based on strong forcing
+%451
