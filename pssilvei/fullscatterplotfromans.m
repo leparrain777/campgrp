86730e53c498a8d3cbde4978e5664caddbb0452d
leparrain777 <30747873+leparrain777@users.noreply.github.com>
2019-08-12 13:03:33 -0700

    beautiful

--- pssilvei/fullscatterplotfromans.m
+++ pssilvei/fullscatterplotfromans.m
@@ -1,4 +1,4 @@
-figure(2)
+figure(figurecounter)
 clf
 scatter3(ans(:,5),ans(:,1),ans(:,3),10.^(ans(:,2)/300),ans(:,4)-mean(ans(:,4)))
-set(gca,'YLim',[0 6.5e19],'ZLim',[160 360])
+set(gca,'YLim',[0 6.5e19],'ZLim',[-100 0])
