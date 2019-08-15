52fa027ee377d2f204176dfb39c49a53d979ad4b
leparrain777 <30747873+leparrain777@users.noreply.github.com>
2019-08-15 13:26:13 -0700

    stuff

--- pssilvei/fullscatterplotfromans.m
+++ pssilvei/fullscatterplotfromans.m
@@ -1,7 +1,6 @@
-figure(3)
-clf
-scatter3(ans(500:end,5),ans(500:end,1),ans(500:end,3),10.^(ans(500:end,2)/300),ans(500:end,4)-mean(ans(500:end,4)))
+figure
+%scatter3(ans(500:end,5),ans(500:end,1),ans(500:end,3),10.^(ans(500:end,2)/300),ans(500:end,4)-mean(ans(500:end,4)))
 set(gca,'YLim',[0 6.5e19])%,'ZLim',[160 380])
-hold on
+%hold on
 plot3(ans(500:end,5),ans(500:end,1),ans(500:end,3))
-hold off
+%hold off
